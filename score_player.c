4d63c99d205449b4f1f4eedae36f9c2c22da4f5a
Van Roosbroeck Johnny <johnny.van.roosbroeck@telenetgroup.be>
2026-02-09 09:05:59 +0100

    score_player

---
+++ score_player.c
@@ -0,0 +1,15 @@
+/* Include Files */
+#define _INC_OLE
+#include <windows.h>
+#undef	_INC_OLE
+#include <tchar.h>
+#include <stdio.h>
+
+#include "general.h"
+#include "Memory.h"
+#include "String.h"
+#include "Message.h"
+#include "font.h"
+#include "score_player.h"
+
+#include "resource.h"
---
+++ score_player.h
@@ -0,0 +1,14 @@
+/*
+ * n01
+ *
+ * score_player.h
+ *
+ */
+
+#ifndef _INC_SCORE_PLAYER_H
+#define _INC_SCORE_PLAYER_H
+
+/* Include Files */
+#include "general.h"
+
+#endif
