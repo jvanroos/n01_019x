dcfc5b88501d174bfb356ad09c38ae5e9c8e3427
Van Roosbroeck Johnny <johnny.van.roosbroeck@telenetgroup.be>
2026-02-11 23:56:58 +0100

    game options continued

--- resource.h
+++ resource.h
@@ -1,5 +1,10 @@
 // INCLUDE FILE 
 
+
+#ifndef IDC_STATIC
+#define IDC_STATIC (-1)
+#endif
+
 #define IDR_ACCELERATOR                 101
 #define IDR_MENU_MAIN  					107
 #define IDD_DIALOG_GAME_OPTION			108
