ee456c3529cc7c83fece3881356d68765d0ad2bc
Van Roosbroeck Johnny <johnny.van.roosbroeck@telenetgroup.be>
2026-07-27 15:28:04 +0200

    drawing more lines

--- score_list.c
+++ score_list.c
@@ -308,6 +308,18 @@ static BOOL draw_background(const DRAW_BUFFER *bf, const int first)
 	}
 	SelectObject(bf->back_dc, ret_pen);
 
+	ret_pen = SelectObject(bf->back_dc, bf->separate_bold_pen);
+
+	MoveToEx(bf->back_dc, -1, bf->header_height - 1, NULL);
+	LineTo(bf->back_dc, bf->back_width, bf->header_height - 1);
+
+	MoveToEx(bf->back_dc, bf->score_right[0] + 1, 0, NULL);
+	LineTo(bf->back_dc, bf->score_right[0] + 1, bf->back_height);
+
+	MoveToEx(bf->back_dc, bf->score_left[0] - 1, 0, NULL);
+	LineTo(bf->back_dc, bf->score_left[0] - 1, bf->back_height);
+
+	SelectObject(bf->back_dc, ret_pen);
 	return TRUE;
 }
 
